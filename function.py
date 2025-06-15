dfaf2d9f20ff1f45884f494c3b7b55afc929829a
sankhadip-roy <sankhadiproy23@gmail.com>
2025-06-15 23:35:22 +0530

    disease classes fix

--- function.py
+++ function.py
@@ -3,39 +3,35 @@ from tensorflow.keras.preprocessing.image import img_to_array
 import cv2
 import numpy as np
 
-classes = ['Pepper__bell___Bacterial_spot', 'Pepper__bell___healthy',
- 'Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy',
- 'Tomato_Bacterial_spot', 'Tomato_Early_blight', 'Tomato_Late_blight',
- 'Tomato_Leaf_Mold', 'Tomato_Septoria_leaf_spot',
- 'Tomato_Spider_mites_Two_spotted_spider_mite', 'Tomato__Target_Spot',
- 'Tomato__Tomato_YellowLeaf__Curl_Virus', 'Tomato__Tomato_mosaic_virus',
- 'Tomato_healthy']
+classes = ['Potato Healthy', 'Pepper Bell Healthy', 'Tomato Leaf Mold', 'Tomato Spider Mites - Two Spotted Spider Mite', 'Tomato Early Blight', 'Tomato Yellow Leaf Curl Virus', 'Potato Late blight',
+           'Tomato Late Blight', 'Tomato Healthy', 'Potato Early Blight', 'Tomato Target Spot', 'Tomato Bacterial Spot', 'Tomato Mosaic Virus', 'Pepper Bell Bacterial Spot', 'Tomato Septoria Leaf Spot']
+
 
 def convert_image_to_array(image_dir):
     try:
         image = cv2.imread(image_dir)
-        if image is not None :
+        if image is not None:
             image = cv2.resize(image, tuple((256, 256)))
             return img_to_array(image)
-        else :
+        else:
             return np.array([])
     except Exception as e:
         print(f"Error : {e}")
         return None
 
+
 def evaluate(image_location):
     loaded_model = load_model(f"./model.h5")
-    
-    im=convert_image_to_array(image_location)
+
+    im = convert_image_to_array(image_location)
     np_image_li = np.array(im, dtype=np.float16) / 255.0
     npp_image = np.expand_dims(np_image_li, axis=0)
 
     result = loaded_model.predict(npp_image)
 
-    itemindex = np.where(result==np.max(result))
+    itemindex = np.where(result == np.max(result))
     # print(itemindex[1][0]) # for testing
     # print("probability:"+str(np.max(result))) # for testing
-    return classes[itemindex[1][0]] 
-   
-# print(evaluate('<image location>')) # for testing
+    return classes[itemindex[1][0]]
 
+# print(evaluate('<image location>')) # for testing
